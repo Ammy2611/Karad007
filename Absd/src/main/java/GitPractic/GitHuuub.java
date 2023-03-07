b301f6b72b4380d48f5370b7b736356463ae973e
rahul <rahul@TIKUDELL-2011>
2023-03-07 15:01:24 +0530

    updt1

--- Absd/src/main/java/GitPractic/GitHuuub.java
+++ Absd/src/main/java/GitPractic/GitHuuub.java
@@ -6,6 +6,8 @@ public class GitHuuub {public static void main(String[] args) {
 	
 	System.out.println("Check");
 	
+	System.out.println("Velocity");
+	System.out.println("Added");
 	
 	
 	System.out.println("Update");
